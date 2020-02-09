012ca3725b1b8aeef41c0569602210356763fd41
Askar Karasaev <askar.karasaev@gmail.com>
2020-02-09 22:45:25 +0900

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page padding>
+    <input v-model="message" @keyup.esc="clearMessage" @keyup.enter="alertMessage" />
+    <button @click="clearMessage">Clear</button>
+    <h5>{{message}}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "This is Quasar"
+    };
+  },
+  methods: {
+    clearMessage() {
+      this.message = "";
+    },
+    alertMessage() {
+      alert(this.message);
+    }
+  }
+};
+</script>
+
+<style>
+</style>

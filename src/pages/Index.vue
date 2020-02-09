ad9a0d83edd662dcee917aa067ae225823d10067
Askar Karasaev <askar.karasaev@gmail.com>
2020-02-09 23:26:09 +0900

    Showing and Hiding Elements - v-show

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
   <q-page padding>
     <input v-model="message" @keyup.esc="clearMessage" @keyup.enter="alertMessage" />
     <button @click="clearMessage">Clear</button>
-    <h5>{{message}}</h5>
+    <h5 class="border-grey" v-show="message.length">{{message}}</h5>
   </q-page>
 </template>
 
@@ -25,4 +25,7 @@ export default {
 </script>
 
 <style>
+.border-grey {
+  border: 1px solid grey;
+}
 </style>
